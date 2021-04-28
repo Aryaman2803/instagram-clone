513eb670417922f4018d8d0637b626796b7504fb
aryaman2803 <aryamansingh2803@gmail.com>
2021-04-29 01:50:39 +0530

    Initialize

---
+++ src/context/firebase.js
@@ -0,0 +1,15 @@
+import { createContext } from "react";
+
+//* (1) Creating context so that we can use Firebase else where
+//* (2) We initialize the Provider in ReactDOM (index.js)
+//* (3) We import Firebase in Lib folder (firebase.js)
+// provider-------component 1 ------- (firebase init here)
+// -------component 2 -------
+// -------component 3 -------
+// consumer-------component 4 ------- (firebase init here)
+// -------component 5 -------
+// consumer-------component 6 ------- (firebase init here)
+
+const FirebaseContext = createContext(null);
+
+export default FirebaseContext;
---
+++ src/lib/firebase.js
@@ -0,0 +1,23 @@
+import Firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import FirebaseContext from "../context/firebase";
+
+//* Here I want to Import the seed file
+// import { seedDatabase } from "../seed";
+
+const config = {
+  apiKey: "AIzaSyCvrIKxwTNiWu7Ip05RSbTCcXchY8LIzpk",
+  authDomain: "instagram-clone-965cc.firebaseapp.com",
+  projectId: "instagram-clone-965cc",
+  storageBucket: "instagram-clone-965cc.appspot.com",
+  messagingSenderId: "1060488058275",
+  appId: "1:1060488058275:web:1de145ed64355ff9357616",
+};
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = firebase.firestore;
+
+//* here is I will call the seed.js file once
+// seedDatabase(firebase);
+
+export { firebase, FieldValue };

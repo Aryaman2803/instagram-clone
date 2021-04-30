abe1e1642f105831d79826213b5a728e103e0174
aryaman2803 <aryamansingh2803@gmail.com>
2021-05-01 01:29:41 +0530

    Signup firebase

--- src/App.js
+++ src/App.js
@@ -8,12 +8,15 @@ import * as ROUTES from "./constants/routes";
 //We only need the login , signup or dashboard, user profile page.
 
 const Login = lazy(() => import("./pages/login"));
+const SignUp = lazy(() => import("./pages/signup"));
+
 function App() {
   return (
     <Router>
       <Suspense fallback={<p>Loading...</p>}>
         <Switch>
           <Route path={ROUTES.LOGIN} component={Login}></Route>
+          <Route path={ROUTES.SIGN_UP} component={SignUp}></Route>
         </Switch>
       </Suspense>
     </Router>
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -2,7 +2,7 @@
 //To avoid it, this file contains all Routes
 
 export const DASHBOARD = "/";
-export const SIGN_UP = "/signup";
+export const SIGN_UP = "/sign-up";
 export const PROFILE = "/p/:username";
 export const LOGIN = "/login";
 export const NOT_FOUND = "/not-found";

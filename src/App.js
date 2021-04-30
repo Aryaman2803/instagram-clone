aae2ed073f725cadf5f8047cd3921ad0b208438a
aryaman2803 <aryamansingh2803@gmail.com>
2021-04-30 19:44:44 +0530

    Login Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
 
+//Lazy allows to split our huge bundle into chunks
+//If you have everything in one bundle which is large ~5MB which we want to avoid having client download it
+//So we can Code split the Web Pack ~ 0.5MB
+//We only need the login , signup or dashboard, user profile page.
 
+const Login = lazy(() => import("./pages/login"));
 function App() {
   return (
-    <div >
-      Hello
-    </div>
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route path={ROUTES.LOGIN} component={Login}></Route>
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
---
+++ src/constants/routes.js
@@ -0,0 +1,8 @@
+//If we want to change any route, we will need to change it everywhere
+//To avoid it, this file contains all Routes
+
+export const DASHBOARD = "/";
+export const SIGN_UP = "/signup";
+export const PROFILE = "/p/:username";
+export const LOGIN = "/login";
+export const NOT_FOUND = "/not-found";

a6a7a54ec34403e3ac38315c0947fe08e07a8890
aryaman2803 <aryamansingh2803@gmail.com>
2021-05-05 01:54:05 +0530

    timeline hook

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,4 +1,14 @@
+import Skeleton from "react-loading-skeleton";
+import usePhotos from "../hooks/use-photos";
+//* We need to get the logged in user's photo (hook)
+
+//* on loading the photos, we need to use react skeleton
+//* if we have photos, render them (create a post component)
+//*  if the user has no photos, tell them to create some photos
+
 const Timeline = () => {
+  const { photos } = usePhotos();
+  console.log(photos);
   return (
     <div className="container col-span-2 ml-2">
       <p>hello timeline</p>
---
+++ src/hooks/use-photos.js
@@ -0,0 +1,28 @@
+import { useState, useEffect, useContext } from "react";
+import UserContext from "../context/user";
+import { getUserByUserId, getPhotos } from "../services/firebase";
+
+//* We expect this hook will be used for photos
+export default function usePhotos() {
+  const [photos, setPhotos] = useState(null);
+  //* Getting user info from the firebase by UserContext
+  //* Setting uid to userId
+  const {
+    user: { uid: userId = "" },
+  } = useContext(UserContext);
+
+  useEffect(() => {
+    async function getTimelinePhotos() {
+      const [{ following }] = await getUserByUserId(userId);
+      let followedUserPhotos = [];
+      //does the user actually follow people?
+      if (following.length > 0) {
+        followedUserPhotos = await getPhotos(userId, following);
+      }
+      followedUserPhotos.sort((a, b) => b.dateCreated - a.dateCreated);
+      setPhotos(followedUserPhotos);
+    }
+    getTimelinePhotos();
+  }, [userId]);
+  return { photos };
+}

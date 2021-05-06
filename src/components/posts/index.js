28dcc39d95d870ccd764ef4c72ad74407386b9c0
aryaman2803 <aryamansingh2803@gmail.com>
2021-05-07 01:49:07 +0530

    add comments

--- src/components/posts/index.js
+++ src/components/posts/index.js
@@ -2,22 +2,34 @@ import { useRef } from "react";
 import PropTypes from "prop-types";
 import Header from "./header";
 import Image from "./images";
+import Action from "./actions";
+import Footer from "./footer";
+import Comments from "./comments";
 export default function Post({ content }) {
-  const {
-    username,
-    imageSrc,
-    caption,
-    docId,
-    totalLikes,
-    likedPhoto,
-    handleFocus,
-  } = content;
+  console.log(content);
+  const commentInput = useRef(null);
+  const handleFocus = () => commentInput.current.focus();
+
   //*COMPONENTS->
   //-> header ,image, actions (like & comment icons), footer, comments
+
   return (
     <div className="rounded col-span-4 border bg-white border-gray-primary mb-16">
-      <Header username={username} />
-      <Image imageSrc={imageSrc} caption={caption} />
+      <Header username={content.username} />
+      <Image imageSrc={content.imageSrc} caption={content.caption} />
+      <Action
+        docId={content.docId}
+        totalLikes={content.likes.length}
+        likedPhoto={content.userLikedPhoto}
+        handleFocus={handleFocus}
+      />
+      <Footer caption={content.caption} username={content.username} />
+      <Comments
+        docId={content.docId}
+        comments={content.comments}
+        posted={content.dateCreated}
+        commentInput={commentInput}
+      />
     </div>
   );
 }
